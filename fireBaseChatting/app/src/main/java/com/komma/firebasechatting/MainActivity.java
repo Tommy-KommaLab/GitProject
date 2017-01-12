65b4378466aab518fb88e339edfeb0d28a048e35
Tommy <tommykim@komma.co.kr>
2017-01-12 15:10:15 +0900

    no message

--- fireBaseChatting/app/src/main/java/com/komma/firebasechatting/MainActivity.java
+++ fireBaseChatting/app/src/main/java/com/komma/firebasechatting/MainActivity.java
@@ -5,11 +5,32 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView chatlist;
+    private ArrayAdapter adapter;
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private String userName = "";
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +47,63 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        Button sendButton = (Button) findViewById(R.id.sendbtn);
+        editText = (EditText) findViewById(R.id.inputtext);
+        chatlist = (ListView) findViewById(R.id.chatlist);
+
+
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        chatlist.setAdapter(adapter);
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+
+        userName = "user" + new Random().nextInt(10000);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChatData chatData = new ChatData(userName, editText.getText().toString());
+                databaseReference.child("message").push().setValue(chatData);
+                editText.setText("");
+            }
+        });
+
+
+         databaseReference.child("message").addChildEventListener(new ChildEventListener() {
+             @Override
+             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                 ChatData chatData = dataSnapshot.getValue(ChatData.class);
+                 adapter.add(chatData.getUserName() + " : " + chatData.getMessage());
+             }
+
+             @Override
+             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+             }
+
+             @Override
+             public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+             }
+
+             @Override
+             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+             }
+
+             @Override
+             public void onCancelled(DatabaseError databaseError) {
+
+             }
+         });
+
+
     }
 
     @Override
